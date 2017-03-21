9b99d529518f42c410ba6c06e55ca40723053dae
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2017-03-21 17:29:49 -0400

    Update install_irkernel.R

--- r-notebook/install_irkernel.R
+++ r-notebook/install_irkernel.R
@@ -2,7 +2,7 @@
 
 # Step 1: Installing packages and dependencies via supplied binary packages
 update.packages(repos='http://cran.us.r-project.org', ask=FALSE)
-install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'), repos='http://cran.us.r-project.org', dependencies = TRUE)
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest', 'ISwR'), repos='http://cran.us.r-project.org', dependencies = TRUE)
 devtools::install_github('IRkernel/IRkernel')
 
 # Step 2: Making the kernel available to Jupyter
